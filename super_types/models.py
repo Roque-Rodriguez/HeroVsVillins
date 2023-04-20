0b511b6feec1fe461abb9175a0d4a1e1c40fa19f
unknown <Roque.Rodriguez02@gmail.com>
2023-04-20 14:43:34 -0400

    Altered settings + finished all models and serializers

--- super_types/models.py
+++ super_types/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SuperTypes(models.Model):
+    type = models.CharField(max_length=255)
+    
---
+++ supers/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from rest_framework import serializers
+from .models import Super
+
+
+class SuperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SuperTypes
+        fields = ["id", 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+
+        super_types_id = serializers.IntegerField(write_only=True)
