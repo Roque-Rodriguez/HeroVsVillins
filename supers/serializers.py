226a8e9bcb1b14153b72bc0ac73ab2ec67a0e544
unknown <Roque.Rodriguez02@gmail.com>
2023-04-20 17:41:55 -0400

    Updated Views

--- supers/serializers.py
+++ supers/serializers.py
@@ -5,7 +5,7 @@ from .models import Super
 
 class SuperSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SuperTypes
+        model = Super
         fields = ["id", 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
 
         super_types_id = serializers.IntegerField(write_only=True)
